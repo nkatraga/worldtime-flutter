67d5594adc50adec59661d065988c3e07ecad22c
nkatraga <vamsinaveen@gmail.com>
2022-03-24 17:05:54 -0500

    Putting the http call in a try catch block so we don't error out if the api doesn't return a value.

--- lib/services/get_local_time.dart
+++ lib/services/get_local_time.dart
@@ -8,15 +8,23 @@ class LocalTime {
   LocalTime({required this.url});
 
   Future<void> getTime() async {
-    Response myJson =
-        await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
-    Map myJsonData = jsonDecode(myJson.body);
-    String jsonTimeStr = myJsonData['datetime'];
-    String timeOffsetStr = myJsonData['utc_offset'];
-    int timeOffset = int.parse(timeOffsetStr.substring(1, 3));
-    DateTime datetimeThere =
-        DateTime.parse(jsonTimeStr).add(Duration(hours: timeOffset));
+    try {
+      Response myJson =
+      await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+      Map myJsonData = jsonDecode(myJson.body);
+      String jsonTimeStr = myJsonData['datetime'];
+      String timeOffsetStr = myJsonData['utc_offset'];
+      int timeOffset = int.parse(timeOffsetStr.substring(1, 3));
+      DateTime datetimeThere =
+      DateTime.parse(jsonTimeStr).add(Duration(hours: timeOffset));
 
-    localTimeStr = DateFormat.jm().format(datetimeThere);
+      localTimeStr = DateFormat.jm().format(datetimeThere);
+    }
+    catch (e) {
+      print('Caught error: $e');
+      localTimeStr = "Couldn't get time from api";
+
+    }
+
   }
 }
