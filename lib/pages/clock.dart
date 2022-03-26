527fc9c4bdcca7ef2e5d545ffa58af5b364aef73
nkatraga <vamsinaveen@gmail.com>
2022-03-26 14:34:07 -0500

    formatting clock page better

--- lib/pages/clock.dart
+++ lib/pages/clock.dart
@@ -7,73 +7,87 @@ class ClockPage extends StatefulWidget {
 }
 
 class _ClockPageState extends State<ClockPage> {
-
   Map clockData = {};
   final flagImagesLocation = 'assets/images/';
 
   @override
   Widget build(BuildContext context) {
-
     clockData = ModalRoute.of(context)!.settings.arguments as Map;
 
-    String timeImage = (clockData['isDaytime'])? "day.png":"night.png";
-    Color? bgColor = (clockData['isDaytime'])? Colors.blue : Colors.indigo[900];
-
+    String timeImage = (clockData['isDaytime']) ? "day.png" : "night.png";
+    Color? bgColor =
+        (clockData['isDaytime']) ? Colors.blue : Colors.indigo[900];
+    Color? fontColor = (clockData['isDaytime']) ? Colors.black : Colors.grey[400];
 
     return Scaffold(
-      backgroundColor: bgColor,
+        backgroundColor: bgColor,
         body: SafeArea(
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/$timeImage'),
-            fit: BoxFit.cover,
-          )
-        ),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                //color: Colors.blue[500],
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 150,
-                    ),
-                    IconButton(
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/locations');
-                        },
-                        icon: Icon(Icons.house)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/images/$timeImage'),
+              fit: BoxFit.cover,
+            )),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    //color: Colors.blue[500],
+                    child: Column(
                       children: [
-                        CircleAvatar(
-                          backgroundImage: AssetImage(flagImagesLocation+clockData['flagImage']),
+                        const SizedBox(
+                          height: 110,
                         ),
-                        Text(clockData['location'],style: const TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),)
+                        IconButton(
+                            onPressed: () {
+                              Navigator.pushReplacementNamed(
+                                  context, '/locations');
+                            },
+                            icon: Icon(
+                              Icons.edit_location,
+                              color: fontColor,
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage(
+                                  flagImagesLocation + clockData['flagImage']),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              clockData['location'],
+                              style: TextStyle(
+                                fontSize: 50,
+                                color: fontColor,
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          clockData['time'],
+                          style: TextStyle(fontSize: 70, color: fontColor),
+                        )
                       ],
                     ),
-                    Text(
-                      clockData['time'],
-                      style: const TextStyle(fontSize: 50),
-                    )
-                  ],
+                  ),
+                  flex: 2,
                 ),
-              ),
-              flex: 1,
+                Expanded(
+                  child: Container(),
+                  flex: 1,
+                )
+              ],
             ),
-            Expanded(
-              child: Container(
-              ),
-              flex: 1,
-            )
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
