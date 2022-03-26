aa3a2a75becba60024b6bd5005a8aaf99a099e27
nkatraga <vamsinaveen@gmail.com>
2022-03-26 15:27:26 -0500

    Switched map with ListView.builder for locations page.

--- lib/pages/locations.dart
+++ lib/pages/locations.dart
@@ -23,6 +23,12 @@ class _LocationsPageState extends State<LocationsPage> {
     City(url: 'America/New_York', location: 'New York', flagImage: 'usa.png'),
     City(url: 'Asia/Seoul', location: 'Seoul', flagImage: 'south_korea.png'),
     City(url: 'Asia/Jakarta', location: 'Jakarta', flagImage: 'indonesia.png'),
+    City(url: 'Africa/Cairo', location: 'Cairo', flagImage: 'egypt.png'),
+    City(url: 'Africa/Nairobi', location: 'Nairobi', flagImage: 'kenya.png'),
+    City(url: 'Asia/Kolkata', location: 'New Delhi', flagImage: 'india.png'),
+    City(url: 'America/New_York', location: 'New York', flagImage: 'usa.png'),
+    City(url: 'Asia/Seoul', location: 'Seoul', flagImage: 'south_korea.png'),
+    City(url: 'Asia/Jakarta', location: 'Jakarta', flagImage: 'indonesia.png'),
   ];
 
   var location = 'Delhi';
@@ -41,13 +47,22 @@ class _LocationsPageState extends State<LocationsPage> {
       appBar: AppBar(
         title: const Text("Choose Location"),
       ),
-      body: Column(
+     /* body: Column(
         children: cities.map((e) => GestureDetector(
           onTap:() {
             Navigator.pushReplacementNamed(context, '/loading',arguments: {'location': e});
             },
             child: LocationCard(city: e))).toList()
-      )
+      )*/
+      body: ListView.builder(
+        itemCount: cities.length,
+          itemBuilder: (context, index){
+        return GestureDetector(
+            onTap:() {
+          Navigator.pushReplacementNamed(context, '/loading',arguments: {'location': cities[index]});
+        },
+            child: LocationCard(city: cities[index],) );
+      }),
     );
   }
 }
